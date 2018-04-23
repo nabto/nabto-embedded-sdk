8a719ac5e87dce12723848ee907efa791b8e420d
fusionfoxy <thom2287@gmail.com>
2018-04-23 15:37:25 +0200

    log levels

--- src/platform/nabto_logging_test.c
+++ src/platform/nabto_logging_test.c
@@ -7,6 +7,11 @@
 void nabto_logging_test()
 {
     nabto_log.log=&unix_log;
-    NABTO_LOG_INFO(42, "This is %s: %d", "a number", 24);
+    NABTO_LOG_FATAL(42, "FATAL: This is %s: %d", "a number", 24);
+    NABTO_LOG_ERROR(42, "ERROR: This is %s: %d", "a number", 24);
+    NABTO_LOG_WARN(42, "WARN: This is %s: %d", "a number", 24);
+    NABTO_LOG_INFO(42, "INFO: This is %s: %d", "a number", 24);
+    NABTO_LOG_DEBUG(42, "DEBUG: This is %s: %d", "a number", 24);
+    NABTO_LOG_TRACE(42, "TRACE: This is %s: %d", "a number", 24);
     NABTO_TEST_CHECK(true);
 }
