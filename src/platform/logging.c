d9d45648a30cb04584adebb4608ab14797865ba0
fusionfoxy <thom2287@gmail.com>
2018-04-24 08:48:36 +0200

    file naming convention

--- src/platform/logging.c
+++ src/platform/logging.c
@@ -1,80 +1,78 @@
-#include <platform/nabto_logging.h>
-#include <stdio.h>
+#include <platform/logging.h>
 
 void nabto_default_log(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, va_list args) {
-    printf("HERE\n");
 }
 struct nabto_logging nabto_log = { &nabto_default_log };
 
 #ifdef HAS_NO_VARIADIC_MACROS
-void fatal_adapter(uint32_t module, const char* fmt, ...) {
+void nabto_fatal_adapter(uint32_t module, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_FATAL, "", 0, "", fmt, args);
     va_end(args);
 }
-void error_adapter(uint32_t module, const char* fmt, ...) {
+void nabto_error_adapter(uint32_t module, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_ERROR, "", 0, "", fmt, args);
     va_end(args);
 }
-void warn_adapter(uint32_t module, const char* fmt, ...) {
+void nabto_warn_adapter(uint32_t module, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_WARN, "", 0, "", fmt, args);
     va_end(args);
 }
-void info_adapter(uint32_t module, const char* fmt, ...) {
+void nabto_info_adapter(uint32_t module, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_INFO, "", 0, "", fmt, args);
     va_end(args);
 }
-void debug_adapter(uint32_t module, const char* fmt, ...) {
+void nabto_debug_adapter(uint32_t module, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_DEBUG, "", 0, "", fmt, args);
     va_end(args);
 }
-void trace_adapter(uint32_t module, const char* fmt, ...) {
+void nabto_trace_adapter(uint32_t module, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_TRACE, "", 0, "", fmt, args);
     va_end(args);
 }
 #else
-void fatal_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
+void nabto_fatal_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_FATAL, module, line, file, fmt, args);
     va_end(args);
 }
-void error_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
+void nabto_error_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_ERROR, module, line, file, fmt, args);
     va_end(args);
 }
-void warn_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
+void nabto_warn_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_WARN, module, line, file, fmt, args);
     va_end(args);
 }
-void info_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
+void nabto_info_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_INFO, module, line, file, fmt, args);
     va_end(args);
 }
-void debug_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
+void nabto_debug_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_DEBUG, module, line, file, fmt, args);
     va_end(args);
 }
-void trace_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
+void nabto_trace_adapter(uint32_t severity, uint32_t module, uint32_t line, const char* file, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     nabto_log.log(NABTO_LOG_SEVERITY_TRACE, module, line, file, fmt, args);
