cd2567a2ba5301f70a137ef15b4d69a96f563bf6
fusionfoxy <thom2287@gmail.com>
2019-01-16 15:52:23 +0100

    select based unix socket

--- src/modules/udp/select_unix/nm_select_unix.h
+++ src/modules/udp/select_unix/nm_select_unix.h
@@ -7,4 +7,21 @@
 
 void nm_select_unix_init(struct np_platform *pl);
 
+
+/** defined here for testing purposes **/
+void nm_select_unix_async_create(np_udp_socket_created_callback cb, void* data);
+void nm_select_unix_async_bind_port(uint16_t port, np_udp_socket_created_callback cb, void* data);
+void nm_select_unix_async_send_to(struct np_udp_send_context* ctx);
+void nm_select_unix_async_recv_from(np_udp_socket* socket,
+                                    np_udp_packet_received_callback cb, void* data);
+void nm_select_unix_cancel_recv_from(np_udp_socket* socket);
+void nm_select_unix_cancel_send_to(struct np_udp_send_context* socket);
+enum np_ip_address_type nm_select_unix_get_protocol(np_udp_socket* socket);
+uint16_t nm_select_unix_get_local_port(np_udp_socket* socket);
+void nm_select_unix_async_destroy(np_udp_socket* socket, np_udp_socket_destroyed_callback cb, void* data);
+int nm_select_unix_inf_wait();
+int nm_select_unix_timed_wait(uint32_t ms);
+void nm_select_unix_read(int nfds);
+
+
 #endif // NM_SELECT_UNIX_H
--- src/modules/udp/select_unix/nm_select_unix_test.c
+++ src/modules/udp/select_unix/nm_select_unix_test.c
@@ -1,7 +1,122 @@
 #include "nm_select_unix.h"
 
+#include <modules/communication_buffer/nm_unix_communication_buffer.h>
+#include <modules/logging/nm_unix_logging.h>
+#include <modules/timestamp/nm_unix_timestamp.h>
+
+#include <platform/np_platform.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+struct np_platform pl;
+np_udp_socket* sock;
+np_udp_socket* sock2;
+np_udp_socket* sock3;
+struct np_udp_send_context sendCtx1;
+struct np_udp_send_context sendCtx2;
+uint32_t counter = 0;
+void sockSendCtx1(np_udp_packet_sent_callback cb);
+void sock3Created(const np_error_code ec, np_udp_socket* socket, void* data);
+
+void sendCb1(const np_error_code ec, void* data)
+{
+    NABTO_LOG_ERROR(0, "    sendCb1");
+    if (counter < 100) {
+        sockSendCtx1(&sendCb1);
+    }
+//    sockSendCtx1(&sendCb1);
+//    sockSendCtx1(NULL);
+//    sockSendCtx1(NULL);
+}
+
+void sockSendCtx1(np_udp_packet_sent_callback cb)
+{
+    sendCtx1.sock = sock;
+    sendCtx1.ep.port = 4242;
+    sendCtx1.ep.ip.type = NABTO_IPV4;
+    sendCtx1.ep.ip.v4.addr[0] = 127;
+    sendCtx1.ep.ip.v4.addr[1] = 0;
+    sendCtx1.ep.ip.v4.addr[2] = 0;
+    sendCtx1.ep.ip.v4.addr[3] = 1;
+    memcpy(pl.buf.start(sendCtx1.buffer), &counter, 4);
+    sendCtx1.bufferSize = 4;
+    sendCtx1.cb = cb;
+    counter++;
+    nm_select_unix_async_send_to(&sendCtx1);
+}
+
+void sock2Recv(const np_error_code ec, struct np_udp_endpoint ep,
+               np_communication_buffer* buffer, uint16_t bufferSize,
+               void* data)
+{
+    NABTO_LOG_ERROR(0, "  recv from sock2: %u", *((uint32_t*)pl.buf.start(buffer)));
+//    NABTO_LOG_BUF(0, pl.buf.start(buffer), bufferSize);
+    nm_select_unix_async_recv_from(sock2, &sock2Recv, NULL);
+}
+
+void sock2Created(const np_error_code ec, np_udp_socket* socket, void* data)
+{
+    NABTO_LOG_INFO(0, "socket2 created");
+    sock2 = socket;
+    sockSendCtx1(&sendCb1);
+    nm_select_unix_async_recv_from(sock2, &sock2Recv, NULL);
+}
+
+void sockCreated(const np_error_code ec, np_udp_socket* socket, void* data)
+{
+    NABTO_LOG_INFO(0, "socket created");
+    sock = socket;
+    nm_select_unix_async_bind_port(4242, &sock2Created, NULL);
+}
+
+void sock3DestroyedCb(const np_error_code ec, void* data)
+{
+    NABTO_LOG_INFO(0, "socket3 destroyed");
+    if (counter >= 100) {
+        return;
+    }
+    nm_select_unix_async_create(&sock3Created, NULL);
+}
+
+void sock3Created(const np_error_code ec, np_udp_socket* socket, void* data)
+{
+    NABTO_LOG_INFO(0, "socket3 created");
+    sock3 = socket;
+    nm_select_unix_async_destroy(sock3, &sock3DestroyedCb, NULL);
+}
 
 int main()
 {
-    
+    int nfds;
+    np_platform_init(&pl);
+    nm_unix_log_init();
+    nm_unix_comm_buf_init(&pl);
+    nm_unix_ts_init(&pl);
+    nm_select_unix_init(&pl);
+    NABTO_LOG_INFO(0, "main");
+
+    sendCtx1.buffer = pl.buf.allocate();
+    sendCtx2.buffer = pl.buf.allocate();
+
+    nm_select_unix_async_create(&sockCreated, NULL);
+    nm_select_unix_async_create(&sock3Created, NULL);
+
+    while(true) {
+        np_event_queue_execute_all(&pl);
+        if (!np_event_queue_is_event_queue_empty(&pl)) {
+            NABTO_LOG_ERROR(0, "Event queue not empty after emptying");
+        }
+        if (np_event_queue_has_timed_event(&pl)) {
+            uint32_t ms = np_event_queue_next_timed_event_occurance(&pl);
+            if (ms == 0) {
+                NABTO_LOG_ERROR(0, "ms was 0 ");
+                ms = 1;
+            }
+            nfds = nm_select_unix_timed_wait(ms);
+        } else {
+            nfds = nm_select_unix_inf_wait();
+        }
+        nm_select_unix_read(nfds);
+    }
 }
