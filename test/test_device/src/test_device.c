71434e4c4945dd78cb94de905f23389a0384dac1
Michael <mkm@nabto.com>
2019-05-14 15:51:53 +0200

    take key file as argument

--- test/test_device/src/test_device.c
+++ test/test_device/src/test_device.c
@@ -7,9 +7,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define MAX_KEY_PEM_SIZE 1024
+#define MAX_CRT_PEM_SIZE 1024
+
 struct config {
     const char* productId;
     const char* deviceId;
+    const char* keyFile;
+    char keyPemBuffer[MAX_KEY_PEM_SIZE];
+    char crtPemBuffer[MAX_CRT_PEM_SIZE];
 };
 
 static struct config config;
@@ -23,22 +29,25 @@ void print_help(const char* message)
         printf(NEWLINE);
     }
     printf("test_device" NEWLINE);
-    printf(" USAGE test_device -p <productId> -d <deviceId>" NEWLINE);
+    printf(" USAGE test_device -p <productId> -d <deviceId> -k <keyfile>" NEWLINE);
 }
 
 bool parse_args(int argc, const char** argv)
 {
     const char* productId;
     const char* deviceId;
+    const char* keyFile;
 
     const char* helpLong[] = { "help", 0 };
     const char* productLong[] = { "product", 0 };
     const char* deviceLong[] = { "device", 0 };
+    const char* keyFileLong[] = { "keyfile", 0 };
 
     const struct { int key; int format; const char* shortName; const char*const* longNames; } opts[] = {
         { 1, GOPT_NOARG, "h", helpLong },
         { 2, GOPT_ARG, "p", productLong },
         { 3, GOPT_ARG, "d", deviceLong },
+        { 4, GOPT_ARG, "k", keyFileLong },
         {0,0,0,0}
     };
 
@@ -62,6 +71,13 @@ bool parse_args(int argc, const char** argv)
         return false;
     }
 
+    if (gopt_arg(options, 4, &keyFile)) {
+        config.keyFile = keyFile;
+    } else {
+        print_help("Missing key filename");
+        return false;
+    }
+
     return true;
 }
 
@@ -70,44 +86,34 @@ bool file_exists(const char* filename)
     return (access(filename, R_OK) == 0);
 }
 
-bool test_if_certs_exists(const char* crtFilename, const char* keyFilename) {
-    if (file_exists(keyFilename) &&
-        file_exists(crtFilename))
-    {
-        return true;
+bool load_key_from_file(const char* filename)
+{
+    FILE* f;
+    f = fopen(filename, "r");
+    if (f == NULL) {
+        return false;
     }
-    return false;
+
+    // if the read failed the key is invalid and we will fail later.
+    fread(config.keyPemBuffer, 1, MAX_KEY_PEM_SIZE, f);
+
+    return true;
 }
 
 int main(int argc, const char** argv)
 {
-    char keyFilename[128];
-    char crtFilename[128];
-    memset(keyFilename, 0, 128);
-    memset(crtFilename, 0, 128);
-
     memset(&config, 0, sizeof(struct config));
     if (!parse_args(argc, argv)) {
         exit(1);
     }
 
-    snprintf(keyFilename, 127, "%s_%s.key", config.productId, config.deviceId);
-    snprintf(crtFilename, 127, "%s_%s.crt", config.productId, config.deviceId);
-
-
-    if (!test_if_certs_exists(crtFilename, keyFilename)) {
-        if (!create_keypair(crtFilename, keyFilename)) {
-            printf("FATAL cannot create a new keypair" NEWLINE);
-            exit(1);
-        }
-
-        printf("Created keypair." NEWLINE);
-        printf("Install the fingerprint of the public key of the device into the basestation." NEWLINE);
-        // todo print the fignerprint
-
-        printf("Start the device again when the fingerprint has been added to the basestation." NEWLINE);
+    if (!file_exists(config.keyFile)) {
+        // TODO generate key with this application.
+        printf("Missing keyfile." NEWLINE);
+        printf("Generate a new keyfile with: openssl ecparam -genkey -name prime256v1 -out <filename>.pem" NEWLINE);
         exit(1);
     }
+    
 
     // TODO read crt and key
     // TODO start a device
