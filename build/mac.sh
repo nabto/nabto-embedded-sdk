36782accdce526d08bd8fae6e2c2e8e265493d9c
Michael Madsen <mkm@nabto.com>
2019-06-14 13:45:20 +0200

    added mac build script

---
+++ build/mac.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+SRC_DIR=${DIR}/..
+
+BUILD_DIR=${DIR}/../../build/nabto-embedded-sdk
+mkdir -p ${BUILD_DIR}
+
+INSTALL_DIR=${DIR}/../../artifacts/nabto-embedded-sdk
+
+cd ${BUILD_DIR}
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${SRC_DIR}
