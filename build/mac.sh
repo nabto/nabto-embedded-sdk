d1759001e07f3e7fdf12d631d83af25f8257faae
Michael Madsen <mkm@nabto.com>
2019-06-14 13:46:20 +0200

    build and install

--- build/mac.sh
+++ build/mac.sh
@@ -11,4 +11,5 @@ INSTALL_DIR=${DIR}/../../artifacts/nabto-embedded-sdk
 
 cd ${BUILD_DIR}
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${SRC_DIR}
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${SRC_DIR} || exit 1
+make -j install || exit 1
