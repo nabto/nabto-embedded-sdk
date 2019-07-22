0dd7929c85ccf1d43e88bb37fcf703a15adddd9f
Michael Madsen <mkm@nabto.com>
2019-07-22 19:37:19 +0200

    dump wip

--- src/core/nc_iam_dump.c
+++ src/core/nc_iam_dump.c
@@ -128,27 +128,14 @@ void nc_iam_dump_policies(struct nc_iam* iam, CborEncoder* encoder)
 
 
 
-np_error_code nc_iam_load(struct nc_iam* iam, void* cbor, size_t cborLength)
+bool nc_iam_load_policies(struct nc_iam* iam, CborValue* policies)
 {
-    CborParser parser;
-    CborValue map;
-    cbor_parser_init(cbor, cborLength, 0, &parser, &map);
-
-
-    CborValue policies;
-    // map with users, roles and policies
-    if (!cbor_value_is_map(&map)) {
-        return false;
-    }
-
-    cbor_value_map_find_value(&map, "Policies", &policies);
-
-    if (!cbor_value_is_map(&policies)) {
+    if (!cbor_value_is_map(policies)) {
         return false;
     }
 
     CborValue policy;
-    cbor_value_enter_container(&policies, &policy);
+    cbor_value_enter_container(policies, &policy);
 
     while (!cbor_value_at_end(&policy)) {
         char buffer[33];
@@ -176,8 +163,126 @@ np_error_code nc_iam_load(struct nc_iam* iam, void* cbor, size_t cborLength)
         nc_iam_cbor_policy_create(iam, buffer, start, policyLength);
     }
 
-    cbor_value_leave_container(&policies, &policy);
+    cbor_value_leave_container(policies, &policy);
+    return true;
+}
+
+bool nc_iam_load_role(struct nc_iam* iam, const char* roleName, CborValue* role)
+{
+    nc_iam_create_role(iam, roleName);
+    if (!cbor_value_is_array(role)) {
+        return false;
+    }
+    CborValue policyName;
+    cbor_value_enter_container(role, &policyName);
+    while(!cbor_value_at_end(&policyName)) {
+        char buffer[33];
+        memset(buffer, 0, 33);
+        if (!cbor_value_is_text_string(&policyName)) {
+            return false;
+        }
+        size_t stringLength;
+        if (!cbor_value_calculate_string_length(&policyName, &stringLength) || stringLength > 32) {
+            return false;
+        }
+
+        size_t len = 33;
+        cbor_value_copy_text_string(&policyName, buffer, &len, &policyName);
+        nc_iam_role_add_policy(iam, roleName, buffer);
+        //cbor_value_advance(&policyName);
+    }
+    cbor_value_leave_container(role, &policyName);
+    return true;
+}
+
+bool nc_iam_load_roles(struct nc_iam* iam, CborValue* roles)
+{
+    if (!cbor_value_is_map(roles)) {
+        return false;
+    }
+
+    CborValue role;
+    cbor_value_enter_container(roles, &role);
+    while(!cbor_value_at_end(&role)) {
+        char buffer[33];
+        memset(buffer, 0, 33);
+        if (!cbor_value_is_text_string(&role)) {
+            return false;
+        }
+        size_t stringLength;
+        if (!cbor_value_calculate_string_length(&role, &stringLength) || stringLength > 32) {
+            return false;
+        }
+        CborValue next;
+        size_t len = 33;
+        cbor_value_copy_text_string(&role, buffer, &len, &next);
+        cbor_value_advance(&role);
+
+        nc_iam_load_role(iam, buffer, &role);
+
+    }
+    cbor_value_leave_container(roles, &role);
+    return true;
+}
 
 
-    return NABTO_EC_NOT_IMPLEMENTED;
+
+bool nc_iam_load_user(struct nc_iam* iam, const char* userName, CborValue* user)
+{
+    if (!cbor_value_is_map(user)) {
+        return false;
+    }
+
+    CborValue roles;
+    cbor_value_map_find_value(user, "Roles", &roles);
+    if (!cbor_value_is_array(&roles)) {
+        return false;
+    }
+
+    CborValue role;
+    cbor_value_enter_container(roles, &role);
+
+    while(!cbor_value_at_end(&role)) {
+        char role[33];
+    }
+
+    cbor_value_leave_container(roles, &role);
+    return true;
+}
+
+bool nc_iam_load_users(struct nc_iam* iam, CborValue* users)
+{
+    if (!cbor_value_is_map(users)) {
+        return false;
+    }
+    CborValue user;
+    cbor_value_enter_container(users, &user);
+
+    cbor_value_leave_container(users, &user);
+    return true;
+}
+
+np_error_code nc_iam_load(struct nc_iam* iam, void* cbor, size_t cborLength)
+{
+    CborParser parser;
+    CborValue map;
+    cbor_parser_init(cbor, cborLength, 0, &parser, &map);
+
+
+    CborValue policies;
+    // map with users, roles and policies
+    if (!cbor_value_is_map(&map)) {
+        return false;
+    }
+
+    cbor_value_map_find_value(&map, "Policies", &policies);
+    nc_iam_load_policies(iam, &policies);
+
+    CborValue roles;
+    cbor_value_map_find_value(&map, "Roles", &roles);
+    nc_iam_load_roles(iam, &roles);
+
+
+
+    return NABTO_EC_OK;
 }
