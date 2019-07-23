faa8f69cb47a387a9ad016d961d2ec3585fbf94a
Michael <mkm@nabto.com>
2019-07-23 13:10:47 +0200

    mdns experimental

--- include/nabto/nabto_device_experimental.h
+++ include/nabto/nabto_device_experimental.h
@@ -6,6 +6,19 @@
 extern "C" {
 #endif
 
+/**
+ * Starts the optional internal mdns responder/server. The mdns
+ * responder can be used if the system does not provide a mdns
+ * implementation.
+ *
+ * Before starting the mdns responder, the product id, and device id
+ * has to be set.
+ *
+ * The responder is stopped when the device is closed.
+ */
+NabtoDeviceError nabto_device_mdns_start(NabtoDevice* device);
+
+
 #ifdef __cplusplus
 } // extern c
 #endif
