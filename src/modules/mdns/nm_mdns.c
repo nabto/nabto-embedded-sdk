199451f42968d1c08cdfdacacb8f2ea1ae3eecd1
Michael Madsen <mkm@nabto.com>
2019-07-26 15:23:42 +0200

    use multicast address!

--- src/modules/mdns/nm_mdns.c
+++ src/modules/mdns/nm_mdns.c
@@ -5,7 +5,7 @@ static void nm_mdns_socket_opened_v4(const np_error_code ec, np_udp_socket* sock
 static void nm_mdns_recv_packet_v4(struct nm_mdns* mdns);
 static void nm_mdns_packet_received_v4(const np_error_code ec, struct np_udp_endpoint ep,
                                     np_communication_buffer* buffer, uint16_t bufferSize, void* userData);
-static void nm_mdns_send_packet_v4(struct nm_mdns* mdns, struct np_udp_endpoint ep);
+static void nm_mdns_send_packet_v4(struct nm_mdns* mdns);
 static void nm_mdns_packet_sent_v4(const np_error_code ec, void* userData);
 
 static void nm_mdns_socket_opened_v6(const np_error_code ec, np_udp_socket* socket, void* userData);
@@ -109,17 +109,22 @@ void nm_mdns_packet_received_v4(const np_error_code ec, struct np_udp_endpoint e
         if (nabto_mdns_server_handle_packet(&mdns->mdnsServer,
                                             pl->buf.start(buffer), bufferSize))
         {
-            nm_mdns_send_packet_v4(mdns, ep);
+            nm_mdns_send_packet_v4(mdns);
             return;
         }
     }
     nm_mdns_recv_packet_v4(mdns);
 }
 
-void nm_mdns_send_packet_v4(struct nm_mdns* mdns, struct np_udp_endpoint ep)
+void nm_mdns_send_packet_v4(struct nm_mdns* mdns)
 {
     struct np_platform* pl = mdns->pl;
     size_t written;
+    struct np_udp_endpoint ep;
+    ep.ip.type = NABTO_IPV4;
+    ep.port = 5353;
+    uint8_t addr[] = { 0xe0, 0x00, 0x00, 0xfb };
+    memcpy(ep.ip.v4.addr,addr, 4);
     uint16_t port = mdns->getPort(mdns->getPortUserData);
     if (port > 0) {
         if (nabto_mdns_server_build_packet(&mdns->mdnsServer, port, pl->buf.start(mdns->sendBufferv4), pl->buf.size(mdns->sendBufferv4), &written)) {
