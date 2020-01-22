441c8a77bf3b6586e6b321084d1f9ba87a04fd10
Michael Madsen <mkm@nabto.com>
2020-01-22 12:10:51 +0100

    fixed example

--- examples/heat_pump/src/main.cpp
+++ examples/heat_pump/src/main.cpp
@@ -23,7 +23,7 @@ void my_handler(int s){
     printf("Caught signal %d\n",s);
 }
 
-bool init_heat_pump(const std::string& configFile, const std::string& productId, const std::string& deviceId, const std::string& server);
+bool init_heat_pump(const std::string& configFile, const std::string& productId, const std::string& deviceId, const std::string& server, const std::string& clientServerUrl, const std::string& clientServerKey);
 bool run_heat_pump(const std::string& configFile, const std::string& logLevel);
 bool reset_iam(const std::string& configFile);
 
