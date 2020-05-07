7b7760b4e1eae3294af283454f351a752c9eb130
Michael Madsen <mkm@nabto.com>
2020-05-07 12:37:13 +0200

    documented random interface

--- src/platform/np_random.h
+++ src/platform/np_random.h
@@ -6,6 +6,14 @@ extern "C" {
 #endif
 
 struct np_random_module {
+    /**
+     * Generate cryptographic strong random data.
+     *
+     * @param pl  The platform
+     * @param buffer  The buffer to fill with random data
+     * @param bufferLength  The length of the buffer
+     * @return NABTO_EC_OK  iff the buffer is filled with cryptographic strong random data.
+     */
     np_error_code (*random)(struct np_platform* pl, void* buffer, size_t bufferLength);
 };
 
