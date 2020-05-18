9a5940df495fae026012e8e4299f72437b2ada97
Michael Madsen <mkm@nabto.com>
2020-05-18 13:48:51 +0200

    updated dns documentation

--- src/platform/np_dns.h
+++ src/platform/np_dns.h
@@ -21,26 +21,55 @@ struct np_dns_module {
     /**
      * Create a new resolver. A resolver can be used to resolve either
      * ipv4 or ipv6 addresses.
+     *
+     * @param pl  The platform
+     * @param resolver  The resulting resolver.
+     * @return NABTO_EC_OK  iff the object is created.
      */
     np_error_code (*create_resolver)(struct np_platform* pl, struct np_dns_resolver** resolver);
 
     /**
      * Destroy a resolver.
+     *
+     * @param resolver  The resolver to destroy.
      */
     void (*destroy_resolver)(struct np_dns_resolver* resolver);
 
     /**
-     * stop a resolver
+     * stop a resolver. Stopping a resolver makes all the outstanding
+     * dns requests stop.
+     *
+     * @param resolver  The resolver to stop.
      */
     void (*stop)(struct np_dns_resolver* resolver);
 
     /**
      * Resolve ipv4 addresses for the host name.
+     *
+     * The completion event shall be resolved when the dns resolution
+     * has either failed or succeeded.
+     *
+     * @param resolver  The resolver
+     * @param host  The host to resolve.
+     * @param ips  The array to store the resolved ips in.
+     * @param ipsSize  The size of the ips array.
+     * @param ipsResolved  The number of ips put in the the ips array.
+     * @param completionEvent  The completion event.
      */
     void (*async_resolve_v4)(struct np_dns_resolver* resolver, const char* host, struct np_ip_address* ips, size_t ipsSize, size_t* ipsResolved, struct np_completion_event* completionEvent);
 
     /**
      * Resolve ipv6 addresses for the host name.
+     *
+     * The completion event shall be resolved when the dns resolution
+     * has either failed or succeeded.
+     *
+     * @param resolver  The resolver
+     * @param host  The host to resolve.
+     * @param ips  The array to store the resolved ips in.
+     * @param ipsSize  The size of the ips array.
+     * @param ipsResolved  The number of ips put in the the ips array.
+     * @param completionEvent  The completion event.
      */
     void (*async_resolve_v6)(struct np_dns_resolver* resolver, const char* host, struct np_ip_address* ips, size_t ipsSize, size_t* ipsResolved, struct np_completion_event* completionEvent);
 
