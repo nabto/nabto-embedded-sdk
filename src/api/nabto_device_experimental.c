9a02659b07405fce86af969004584025206380ef
Michael Madsen <mkm@nabto.com>
2020-05-29 09:07:36 +0200

    implmented test for invalid key

--- src/api/nabto_device_experimental.c
+++ src/api/nabto_device_experimental.c
@@ -9,6 +9,10 @@
 #include <mbedtls/pk.h>
 
 
+static uint8_t secp2566r1GroupOrder[32] = {
+    0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xBC, 0xE6, 0xFA, 0xAD, 0xA7, 0x17, 0x9E, 0x84, 0xF3, 0xB9, 0xCA, 0xC2, 0xFC, 0x63, 0x25, 0x51
+};
+
 NabtoDeviceError NABTO_DEVICE_API
 nabto_device_set_private_key_secp256r1(NabtoDevice* device, const uint8_t* key, size_t keyLength)
 {
@@ -21,8 +25,10 @@ nabto_device_set_private_key_secp256r1(NabtoDevice* device, const uint8_t* key, 
 
 
     mbedtls_pk_context pk;
+    mbedtls_mpi n; // n is the order of the secp256r1 group
 
     mbedtls_pk_init(&pk);
+    mbedtls_mpi_init(&n);
 
     status =  mbedtls_pk_setup( &pk, pkInfo);
     if (status != 0) {
@@ -42,6 +48,20 @@ nabto_device_set_private_key_secp256r1(NabtoDevice* device, const uint8_t* key, 
 
     mbedtls_mpi_read_binary(&keyPair->d, key, keyLength);
 
+    mbedtls_mpi_read_binary(&n, secp2566r1GroupOrder, 32);
+
+    // valid private keys should be in the range [1,n-1], d != 0 && d < n;
+
+    // test that d is not 0
+    if (mbedtls_mpi_cmp_int(&keyPair->d, 0) == 0) {
+        return NABTO_DEVICE_EC_INVALID_ARGUMENT;
+    }
+    // test that d < n
+    // check that d lesser than n, in this case the cmp function returns -1
+    if (mbedtls_mpi_cmp_mpi(&keyPair->d, &n) != -1) {
+        return NABTO_DEVICE_EC_INVALID_ARGUMENT;
+    }
+
     // Q = dG
 
     {
@@ -60,6 +80,7 @@ nabto_device_set_private_key_secp256r1(NabtoDevice* device, const uint8_t* key, 
         return NABTO_DEVICE_EC_INVALID_STATE;
     }
     mbedtls_pk_free(&pk);
+    mbedtls_mpi_free(&n);
 
     return nabto_device_set_private_key(device, (const char*)buffer);
 }
