b84d9974d76b637f23723b512c3e1bc2d0710b84
Carsten Gregersen <crg@nabto.com>
2020-06-04 16:16:09 +0200

    Added for cmake

--- nabto_modules_files.cmake
+++ nabto_modules_files.cmake
@@ -26,6 +26,12 @@ set(ne_event_queue_src
   ${ne_eventqueue_dir}/nm_event_queue.c
 )
 
+# Nabto communications buffer
+set(ne_communication_buffer_dir ${ne_dir}/src/modules/communication_buffer)
+set(ne_communication_buffer_src
+  ${ne_communication_buffer_dir}/nm_communication_buffer.c
+)
+
 # Nabto common components
 set(ne_nn_common_components ${ne_dir}/nabto-common/components/nn)
 set(ne_nn_util_src
@@ -43,6 +49,21 @@ set(ne_dns_src
   ${ne_dns_dir}/unix/nm_unix_dns.c
 )
 
+# Unix wrappers
+set(ne_unix_dir ${ne_dir}/src/modules/unix/)
+
+# Nabto mdns impl.
+set(ne_mdns_dir ${ne_dir}/src/modules/mdns)
+set(ne_mdns_src
+  ${ne_mdns_dir}/nm_mdns.c
+  ${ne_unix_dir}/nm_unix_mdns.c
+)
+
+# Nabto local ip implementation
+set(ne_localip_src
+  ${ne_unix_dir}/nm_unix_local_ip.c
+)
+
 # Nabto timestamp posix impl.
 set(ne_timestamp_dir ${ne_dir}/src/modules/timestamp)
 set(ne_timestamp_src
@@ -55,5 +76,13 @@ set(ne_select_unix_src
   ${ne_select_unix_dir}/nm_select_unix.c
   ${ne_select_unix_dir}/nm_select_unix_udp.c
   ${ne_select_unix_dir}/nm_select_unix_tcp.c
+)
+
+# Nabto tcp tunnel impl.
+set(ne_tcp_tunnel_dir ${ne_dir}/src/modules/tcp_tunnel)
+set(ne_tcp_tunnel_src
+  ${ne_tcp_tunnel_dir}/nm_tcp_tunnel_connection.c
+  ${ne_tcp_tunnel_dir}/nm_tcp_tunnel_coap.c
+  ${ne_tcp_tunnel_dir}/nm_tcp_tunnel.c
 )
 
