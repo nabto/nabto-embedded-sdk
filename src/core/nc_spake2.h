77811bf37539445572f8078948645ae7c473042f
Michael Madsen <mkm@nabto.com>
2020-08-10 11:43:46 +0200

    limit username length to 32 chars such that it's easier to put into the connection context in the future

--- src/core/nc_spake2.h
+++ src/core/nc_spake2.h
@@ -15,8 +15,7 @@ struct nabto_coap_server_request;
 struct nabto_coap_server_resource;
 struct nc_coap_server_context;
 
-#define NC_SPAKE2_USERNAME_MAX_LENGTH 64
-#define NC_SPAKE2_PASSWORD_MAX_LENGTH 64
+#define NC_SPAKE2_USERNAME_MAX_LENGTH 32
 #define NC_SPAKE2_MAX_PASSWORD_AUTHENTICATION_REQUESTS 6
 
 /**
