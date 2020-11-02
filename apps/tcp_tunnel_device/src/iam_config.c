d1407a2aee5839abd8f91763bf8f2ef830e95b9f
Michael Madsen <mkm@nabto.com>
2020-11-02 18:15:58 +0100

    added set user password to policy

--- apps/tcp_tunnel_device/src/iam_config.c
+++ apps/tcp_tunnel_device/src/iam_config.c
@@ -163,6 +163,7 @@ bool iam_config_create_default_invite(const char* iamConfigFile)
         nm_iam_configuration_statement_add_action(stmt, "IAM:SetUserRole");
         nm_iam_configuration_statement_add_action(stmt, "IAM:ListRoles");
         nm_iam_configuration_statement_add_action(stmt, "IAM:CreateUser");
+        nm_iam_configuration_statement_add_action(stmt, "IAM:SetUserPassword");
         nm_iam_configuration_add_policy(iamConfig, policy);
     }
 
