22f9ce641ea58c35ae44e73b8c19054799fb16ad
mkm <mkm@nabto.com>
2022-06-28 05:41:53 +0000

    add c++ guards

--- src/core/nc_client_connection_dispatch.h
+++ src/core/nc_client_connection_dispatch.h
@@ -5,6 +5,10 @@
 
 #include <nn/llist.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct nc_udp_dispatch_context;
 
 typedef void (*nc_client_connection_dispatch_close_callback)(void* data);
@@ -44,5 +48,9 @@ np_error_code nc_client_connection_dispatch_close_connection(struct nc_client_co
 struct nc_client_connection* nc_client_connection_dispatch_connection_from_ref(struct nc_client_connection_dispatch_context* ctx, uint64_t ref);
 
 bool nc_client_connection_dispatch_validate_connection_id(struct nc_client_connection_dispatch_context* ctx, const uint8_t* connectionId);
+
+#ifdef __cplusplus
+} // extern c
+#endif
 
 #endif
