548499253169d588eace5106633a9d0e373bc575
mkm <mkm@nabto.com>
2022-10-17 12:57:41 +0200

    LOG IAM actions at trace level such that they do not confuse legitimate info level logging

--- src/modules/iam/nm_iam_internal.c
+++ src/modules/iam/nm_iam_internal.c
@@ -66,7 +66,7 @@ bool nm_iam_internal_check_access(struct nm_iam* iam, NabtoDeviceConnectionRef r
         username = "Not Paired";
     }
 
-    NN_LOG_INFO(iam->logger, LOGM, "IAM access from the user: %s, request action: %s, verdict: %s", username, action, verdict?"ALLOW":"DENY");
+    NN_LOG_TRACE(iam->logger, LOGM, "IAM access from the user: %s, request action: %s, verdict: %s", username, action, verdict?"ALLOW":"DENY");
 
     return verdict;
 }
