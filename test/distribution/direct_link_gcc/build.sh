a37d13727ecadab1237708c224fa277d4f9e4a07
Michael <mkm@nabto.com>
2024-08-05 16:35:47 +0200

    created build.sh scripts for distribution tests

--- test/distribution/direct_link_gcc/build.sh
+++ test/distribution/direct_link_gcc/build.sh
@@ -2,24 +2,23 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+SOURCE_DIR=${DIR}/../../..
+BUILD_DIR=${SOURCE_DIR}/build/direct_link_gcc
+INSTALL_DIR=${BUILD_DIR}/install
+
 set -e
 set -v
 
 export CC=gcc
 export CXX=g++
 
-CURRENT_DIR=$(pwd)
+mkdir -p ${BUILD_DIR}
 
-NABTO_EMBEDDED_SDK_SOURCE_DIR=${DIR}/../../..
-NABTO_EMBEDDED_SDK_BUILD_DIR=${CURRENT_DIR}/nabto_embedded_sdk_build
-INSTALL_DIR=${CURRENT_DIR}/install
-
-mkdir -p ${NABTO_EMBEDDED_SDK_BUILD_DIR}
-mkdir -p ${INSTALL_DIR}
-
-cd ${NABTO_EMBEDDED_SDK_BUILD_DIR}
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${NABTO_EMBEDDED_SDK_SOURCE_DIR}
+cd ${BUILD_DIR}
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${SOURCE_DIR}
 make -j 16 install
 
-cd ${CURRENT_DIR}
+cd ${BUILD_DIR}
 gcc -I${INSTALL_DIR}/include -L${INSTALL_DIR}/lib ${DIR}/test.c -lnabto_device -levent_extra -levent_pthreads -levent_core -lnn -lnabto_mdns -lnabto_stream -lnabto_stun -lnabto_coap -ltinycbor -lmbedtls -lmbedcrypto -lmbedx509
+
+./a.out
