13e2916756204f7eaaa00b352b8c2e5adbbbbe0e
Michael <mkm@nabto.com>
2024-08-05 17:26:00 +0200

    fixed mbedtls library build

--- test/mbedtls_as_library/build.sh
+++ test/mbedtls_as_library/build.sh
@@ -13,11 +13,11 @@ NABTO_BUILD_DIR=${DIR}/nabto
 
 mkdir -p ${MBEDTLS_DIR}
 cd ${MBEDTLS_DIR}
-curl -sSL https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/v3.4.0.tar.gz | tar -xzf - --strip-components=1
+curl -sSL https://github.com/Mbed-TLS/mbedtls/releases/download/v3.6.0/mbedtls-3.6.0.tar.bz2 | tar -xjf - --strip-components=1
 
 MBEDTLS_BUILD_DIR=${DIR}/mbedtls_build
 
-mkdir ${MBEDTLS_BUILD_DIR}
+mkdir -p ${MBEDTLS_BUILD_DIR}
 cd ${MBEDTLS_BUILD_DIR}
 cmake -DCMAKE_INSTALL_PREFIX=${MBEDTLS_INSTALL_DIR} -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DUSE_SHARED_MBEDTLS_LIBRARY=On ../mbedtls
 make -j 8
