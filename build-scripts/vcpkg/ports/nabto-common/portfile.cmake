aa9e1d7d0b849bbc12e00c26f66e5055bf6f5179
Michael <mkm@nabto.com>
2024-08-16 12:09:15 +0200

    use find_package for nabtocommon and build c99 test using a cmake preset

--- build-scripts/vcpkg/ports/nabto-common/portfile.cmake
+++ build-scripts/vcpkg/ports/nabto-common/portfile.cmake
@@ -1,14 +1,34 @@
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO nabto/nabto-common
-    REF "95c9cae4b085668c4221f43c0d874d981daba4b1"
-    SHA512 b4b45751df9eecce634f740d61a481625001e0669a250fb72499c760f3b0b46bb51121ca8041e2708f3a3b7e022a62b2b3137dac353e82ea6924fc9b6c2a7da8
-    HEAD_REF "master"
-)
+set(USE_LOCAL_GIT_CHECKOUT OFF)
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-)
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+if (NOT USE_LOCAL_GIT_CHECKOUT)
+
+    vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO nabto/nabto-common
+        REF "4d7329f62eec3e22d1870bcbdb960fb51d5c7ada"
+        SHA512 7b0bbc92f46ff60b7174693bc03f30b591d6a78a12aeeaac8d677036270ca026695ab4a1fd14a9d0ea89b8c02981aed76a24767ac594c2874280f7867c4ea857
+        HEAD_REF "master"
+    )
+
+    vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+    )
+
+else()
+
+    string(TIMESTAMP CURRENT_TIME UTC)
+        configure_file(
+            ${CMAKE_CURRENT_LIST_DIR}/timestamp.txt.in
+            ${CMAKE_CURRENT_LIST_DIR}/timestamp.txt
+            @ONLY)
+
+    vcpkg_cmake_configure(
+        SOURCE_PATH "../../../../../nabto-common"
+    )
+
+endif()
 
 vcpkg_cmake_install()
 
