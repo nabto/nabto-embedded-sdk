9257de58745e8dbba92e78d049158e4135d865cd
Thomas <tfk@debian>
2024-09-16 10:55:32 +0200

    another attempt to fix CI

--- test_cpp/tests/core/watchdog_test.cpp
+++ test_cpp/tests/core/watchdog_test.cpp
@@ -25,9 +25,6 @@ class WatchdogTest {
 
         np_error_code ec = nc_attacher_watchdog_init(&ctx_, pl, &attach_, &watchDogCallback, this);
         BOOST_TEST(ec == NABTO_EC_OK);
-        nc_attacher_watchdog_set_timeout(&ctx_, 50);
-
-        setAttacherState(NC_ATTACHER_STATE_DNS);
     }
 
     ~WatchdogTest() {
@@ -87,6 +84,8 @@ BOOST_AUTO_TEST_SUITE(watchdog)
 BOOST_AUTO_TEST_CASE(watchdog_trigger, *boost::unit_test::timeout(300))
 {
     nabto::test::WatchdogTest test;
+    nc_attacher_watchdog_set_timeout(test.getWatchdogCtx(), 50);
+    test.setAttacherState(NC_ATTACHER_STATE_DNS);
     test.waitForCallback();
     BOOST_TEST(test.getLastEvent() == NC_DEVICE_EVENT_WATCHDOG_FAILURE);
 
@@ -95,6 +94,7 @@ BOOST_AUTO_TEST_CASE(watchdog_trigger, *boost::unit_test::timeout(300))
 BOOST_AUTO_TEST_CASE(watchdog_dont_trigger, *boost::unit_test::timeout(300))
 {
     nabto::test::WatchdogTest test;
+    nc_attacher_watchdog_set_timeout(test.getWatchdogCtx(), 100);
     test.setAttacherState(NC_ATTACHER_STATE_DNS);
     std::this_thread::sleep_for(std::chrono::milliseconds(20));
     test.setAttacherState(NC_ATTACHER_STATE_DNS);
