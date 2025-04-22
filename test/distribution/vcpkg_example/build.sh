9fe0a9034c1460820ef34cbc30b49cb460687d25
Michael Madsen <mkm85@users.noreply.github.com>
2025-04-22 12:15:02 +0200

    Vcpkg howto (#215)
    * example for how to use NabtoEmbeddedSDK with vcpkg
    * updated documentation

---
+++ test/distribution/vcpkg_example/build.sh
@@ -0,0 +1,23 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+SOURCE_DIR=${DIR}/../../..
+BUILD_DIR=${SOURCE_DIR}/build/vcpkg_example
+INSTALL_DIR=${BUILD_DIR}/install
+
+mkdir -p ${BUILD_DIR}
+
+set -v
+set -e
+
+cd ${BUILD_DIR}
+
+#Explanation:
+# * The CMAKE_MODULE_PATH is needed since vcpkg still provides MbedTLS 2 and
+#   tinycbor which does not come with find_package scripts.
+# * The CMAKE_TOOLCHAIN_FILE is set such that the cmake build system will use
+#   vcpkg
+
+cmake -DCMAKE_MODULE_PATH=${SOURCE_DIR}/cmake/vcpkg  -DCMAKE_TOOLCHAIN_FILE=${SOURCE_DIR}/3rdparty/vcpkg/scripts/buildsystems/vcpkg.cmake ${DIR}
+make -j 16
+
+./vcpkg_example
