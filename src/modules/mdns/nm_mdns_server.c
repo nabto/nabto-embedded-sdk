629cbe77363aa324c7fb27fe7a90e0cd16f9b4ad
Thomas <tfk@debian>
2025-04-24 12:36:47 +0200

    fix review comment

--- src/modules/mdns/nm_mdns_server.c
+++ src/modules/mdns/nm_mdns_server.c
@@ -95,7 +95,7 @@ static void instance_deinit(struct nm_mdns_server_instance* instance)
 // initialize the mdns server
 np_error_code nm_mdns_server_init(struct nm_mdns_server* server, struct np_event_queue* eq, struct np_udp* udp, struct nm_mdns_udp_bind* mdnsUdpBind, struct np_local_ip* localIp)
 {
-    server->state = NEW;
+    server->state = NM_MDNS_SERVER_NEW;
     server->eq = *eq;
     server->udp = *udp;
     server->mdnsUdpBind = *mdnsUdpBind;
@@ -144,11 +144,11 @@ void nm_mdns_server_close(struct nm_mdns_server* server, struct np_completion_ev
         np_completion_event_resolve(closedEvent, NABTO_EC_OPERATION_IN_PROGRESS);
         return;
     }
-    if (server->state != RUNNING) {
+    if (server->state != NM_MDNS_SERVER_RUNNING) {
         np_completion_event_resolve(closedEvent, NABTO_EC_OK);
         return;
     }
-    server->state = CLOSED;
+    server->state = NM_MDNS_SERVER_CLOSED;
     server->closedCompletionEvent = closedEvent;
     np_completion_event_reinit(&server->instanceCloseCompletionEvent, v4_closed, server);
     nm_mdns_server_close_instance(&server->v4, &server->instanceCloseCompletionEvent);
@@ -212,7 +212,7 @@ void nm_mdns_server_instance_stop(struct nm_mdns_server* server, struct nm_mdns_
 
 void nm_mdns_server_stop(struct nm_mdns_server* server)
 {
-    server->state = STOPPED;
+    server->state = NM_MDNS_SERVER_STOPPED;
     nm_mdns_server_instance_stop(server, &server->v4);
     nm_mdns_server_instance_stop(server, &server->v6);
 }
@@ -222,15 +222,15 @@ void publish_service(struct np_mdns* obj, uint16_t port, const char* instanceNam
     struct nm_mdns_server* server = obj->data;
 
 
-    if (server->state == NEW) {
-        server->state = RUNNING;
+    if (server->state == NM_MDNS_SERVER_NEW) {
+        server->state = NM_MDNS_SERVER_RUNNING;
         nabto_mdns_server_init(&server->mdnsServer);
 
         nm_mdns_udp_bind_async_ipv4(&server->mdnsUdpBind, server->v4.socket, &server->v4.openedCompletionEvent);
         nm_mdns_udp_bind_async_ipv6(&server->mdnsUdpBind, server->v6.socket, &server->v6.openedCompletionEvent);
     }
 
-    if (server->state == RUNNING) {
+    if (server->state == NM_MDNS_SERVER_RUNNING) {
         // Do not publish from closed/stopped states
         server->port = port;
         nabto_mdns_server_update_info(&server->mdnsServer, instanceName, subtypes, txtItems);
@@ -265,8 +265,8 @@ void nm_mdns_update_local_ips(struct nm_mdns_server* mdns)
 void nm_mdns_socket_opened(const np_error_code ec, void* userData)
 {
     struct nm_mdns_server_instance* instance = userData;
-    if (instance->server->state == STOPPED ||
-        instance->server->state == CLOSED) {
+    if (instance->server->state == NM_MDNS_SERVER_STOPPED ||
+        instance->server->state == NM_MDNS_SERVER_CLOSED) {
         return;
     }
     if (ec == NABTO_EC_OK) {
@@ -279,7 +279,7 @@ void nm_mdns_socket_opened(const np_error_code ec, void* userData)
 
 void nm_mdns_recv_packet(struct nm_mdns_server_instance* instance)
 {
-    if (instance->server->state == RUNNING) {
+    if (instance->server->state == NM_MDNS_SERVER_RUNNING) {
         np_udp_async_recv_wait(&instance->server->udp, instance->socket, &instance->recvWaitCompletionEvent);
     }
 }
@@ -288,8 +288,8 @@ void nm_mdns_packet_recv_wait_completed(const np_error_code ecIn, void* userData
 {
     struct nm_mdns_server_instance* instance = userData;
 
-    if (instance->server->state == STOPPED ||
-        instance->server->state == CLOSED ||
+    if (instance->server->state == NM_MDNS_SERVER_STOPPED ||
+        instance->server->state == NM_MDNS_SERVER_CLOSED ||
         ecIn == NABTO_EC_ABORTED) {
         return;
     }
@@ -348,7 +348,7 @@ void nm_mdns_send_packet(struct nm_mdns_server_instance* instance, uint16_t id, 
 {
     uint16_t port = instance->server->port;
 
-    if (instance->server->state == RUNNING &&
+    if (instance->server->state == NM_MDNS_SERVER_RUNNING &&
         instance->sendBuffer == NULL &&
         port > 0) {
         size_t written = 0;
